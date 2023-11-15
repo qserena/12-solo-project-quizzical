9d45c456f5a6435a6682e8eebc568e9672dcbf26
Tom Serenander <tom@serenander.se>
2023-11-15 11:42:02 +0100

    fetch is called at right moments.

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,34 +1,62 @@
 import { useState, useEffect } from 'react'
+import he from 'he'
+import { nanoid } from 'nanoid'
 
 export default function Quiz() {
 	const [checkResult, setCheckResult] = useState(false)
+	const [questions, setQuestions] = useState([])
 
 	useEffect(() => {
 		let ignore = false
 
-		fetch(
-			'https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple'
-		)
-			.then((res) => res.json())
-			.then((data) => {
-				if (!ignore) {
-					console.log(data)
-				}
-			})
+		// fetch(
+		// 	'https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple'
+		// )
+		if (!checkResult) {
+			fetch('https://api.imgflip.com/get_memes')
+				.then((res) => res.json())
+				.then((data) => {
+					if (!ignore) {
+						// setQuestions(data.results)
+						// composeQuestions(data.results)
+						composeQuestions(data.data.memes)
+						console.log(data)
+					}
+				})
+		}
 
 		return () => {
 			ignore = true
 		}
-	}, [])
+	}, [checkResult])
+
+	function composeQuestions(results) {
+		let newArr = []
+		for (let i = 0; i < 5; i++) {
+			newArr[i] = {
+				...results[i],
+				id: nanoid(),
+			}
+		}
+		console.log(newArr)
+		setQuestions(newArr)
+	}
 
 	function startNewQuiz() {
 		setCheckResult(false)
 	}
+
+	const questionElements = questions.map((question) => (
+		<div>
+			{/* <h2>{he.decode(question.question)}</h2> */}
+			<h2>{he.decode(question.name)}</h2>
+			<hr />
+		</div>
+	))
+
 	return (
 		<div className="quiz--container">
-			<div className="quiz--questions">
-				<p>Questions here</p>
-			</div>
+			<div className="quiz--questions">{questionElements}</div>
 			<div className="quiz--footer">
 				{checkResult ? (
 					<div className="score-container">
